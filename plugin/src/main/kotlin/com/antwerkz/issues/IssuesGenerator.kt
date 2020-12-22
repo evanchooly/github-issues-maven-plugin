65a2e3746ffa7cef966331493d283f7990e10571
evanchooly <jlee@antwerkz.com>
2020-12-22 00:29:35 -0500

    break out issues from PRs fix filtering on ignored labels

--- plugin/src/main/kotlin/com/antwerkz/issues/IssuesGenerator.kt
+++ plugin/src/main/kotlin/com/antwerkz/issues/IssuesGenerator.kt
@@ -1,5 +1,6 @@
 package com.antwerkz.issues
 
+import org.kohsuke.github.GHFileNotFoundException
 import org.kohsuke.github.GHIssue
 import org.kohsuke.github.GHIssueState.ALL
 import org.kohsuke.github.GHIssueState.CLOSED
@@ -13,11 +14,15 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class IssuesGenerator(
-    val repositoryName: String,
+    repositoryName: String,
     version: String,
     var docsUrl: String? = null,
     var javadocUrl: String? = null
 ) {
+    companion object {
+        val excludedLabels = listOf("wontfix", "invalid")
+    }
+
     private var credentials: String? = null
     val github: GitHub by lazy {
         credentials?.let { GitHubBuilder.fromPropertyFile(credentials).build() } ?: GitHub.connect()
@@ -26,9 +31,10 @@ class IssuesGenerator(
     val repository: GHRepository by lazy {
         github.getRepository(repositoryName)
     }
-    val milestone: GHMilestone by lazy { findMilestone() }
+    val milestone: GHMilestone by lazy { repository.findMilestone(this.version) }
     val release: GHRelease by lazy { findRelease() }
     val issues: Map<String, List<GHIssue>> by lazy { groupIssues() }
+    val pullRequests: List<GHIssue> by lazy { listPRs() }
 
     init {
         val credentials = System.getenv("GH_CREDS") ?: System.getProperty("GH_CREDS")
@@ -67,46 +73,61 @@ class IssuesGenerator(
             }
         }
 
-        if (milestone.closedIssues > 0) {
-            notes += "\n### ${milestone.closedIssues} Issues Resolved\n"
+        if (issues.isNotEmpty()) {
+            notes += "\n### ${issues.size} Issues Resolved\n"
             val labels = repository.listLabels().map { it.name to it.color }.toMap()
 
             issues.forEach { (key, issues) ->
                 notes += "#### ![](https://placehold.it/15/${labels[key]}/000000?text=+) ${key.toUpperCase()}\n"
                 issues.forEach { issue ->
-                    val label = if (issue.isPullRequest) "PR" else "Issue"
-                    notes += "* [$label #${issue.number}](${issue.htmlUrl}): ${issue.title}\n"
+                    notes += "* [#${issue.number}](${issue.htmlUrl}): ${issue.title}\n"
                 }
                 notes += "\n"
+            }
+        }
+        if (pullRequests.isNotEmpty()) {
+            notes += "\n### ${pullRequests.size} Pull Requests merged\n"
+            pullRequests.forEach { pr ->
+                notes += "* [#${pr.number}](${pr.htmlUrl}): ${pr.title}\n"
             }
         }
 
         return notes
     }
 
-    private val excludedLabels = listOf("wontfix", "invalid")
     private fun groupIssues(): Map<String, List<GHIssue>> {
-        val filter = repository.listIssues(CLOSED).filter { it.milestone?.number == milestone.number }
-
-        return filter.filter { issue ->
-            issue.labels.map { it.name }.intersect(excludedLabels).isEmpty()
-        }.flatMap {
-            if (it.labels.isEmpty()) listOf("uncategorized" to it)
-            else it.labels.map { label -> label.name to it }
-        }.groupBy({ it.first }, { it.second }).mapValues { it.value.sortedBy { issue -> issue.number } }.toSortedMap()
+        return repository.getIssues(CLOSED, milestone)
+            .filter { !it.isPullRequest }
+            .filter { issue ->
+                issue.labels.map { it.name }.intersect(excludedLabels).isEmpty()
+            }
+            .flatMap {
+                if (it.labels.isEmpty()) listOf("uncategorized" to it)
+                else it.labels.map { label -> label.name to it }
+            }
+            .groupBy({ it.first }, { it.second })
+            .mapValues { it.value.sortedBy { issue -> issue.number } }
+            .toSortedMap()
     }
 
-    private fun findMilestone(): GHMilestone {
-        return repository.findMilestoneByTitle(version)
-            ?: throw IllegalArgumentException("Milestone $version not found")
+    private fun listPRs(): List<GHIssue> {
+        return repository.getIssues(CLOSED, milestone)
+            .filter { it.isPullRequest }
+            .filter {
+                it.labels.intersect(excludedLabels).isEmpty()
+            }
     }
+
     private fun findRelease(): GHRelease {
         val name = "Version $version"
-        return repository.findReleaseByName(name)
-            ?: repository.createRelease(name)
+        return try {
+            repository.findReleaseByName(name)
+        } catch (_: IllegalArgumentException) {
+            repository.createRelease(name)
                 .name(name)
                 .draft(true)
                 .create()
+        }
     }
 
     fun config(file: File): IssuesGenerator {
@@ -115,10 +136,10 @@ class IssuesGenerator(
     }
 }
 
-fun GHRepository?.findReleaseByName(name: String): GHRelease? {
-    return this?.listReleases()?.find { it.name == name }
+fun GHRepository?.findReleaseByName(name: String): GHRelease {
+    return this?.listReleases()?.find { it.name == name } ?: throw IllegalArgumentException("No release found named $name")
 }
 
-fun GHRepository?.findMilestoneByTitle(title: String): GHMilestone? {
-    return this?.listMilestones(ALL)?.find { it.title == title }
+fun GHRepository.findMilestone(title: String): GHMilestone {
+    return listMilestones(ALL)?.find { it.title == title } ?: throw IllegalArgumentException("No milestone found named $title")
 }
--- plugin/src/test/kotlin/com/antwerkz/maven/GitHubIssuesMojoTest.kt
+++ plugin/src/test/kotlin/com/antwerkz/maven/GitHubIssuesMojoTest.kt
@@ -1,20 +1,23 @@
 package com.antwerkz.maven
 
 import com.antwerkz.issues.IssuesGenerator
-import com.antwerkz.issues.findMilestoneByTitle
+import com.antwerkz.issues.findMilestone
 import com.antwerkz.issues.findReleaseByName
-import junit.framework.Assert.assertFalse
-import junit.framework.Assert.assertTrue
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.kohsuke.github.GHFileNotFoundException
+import org.kohsuke.github.GHIssueState.OPEN
 import org.kohsuke.github.GHRepository
+import org.kohsuke.github.GitHub
 import org.kohsuke.github.GitHubBuilder
 import java.io.File
 
 class GitHubIssuesMojoTest {
     companion object {
-        val TEST_REPOSITORY = "issues-tester"
-        val gitHub = GitHubBuilder.fromPropertyFile("../testing-github.properties").build()
+        const val TEST_REPOSITORY = "issues-tester"
+        val gitHub: GitHub = GitHubBuilder
+            .fromPropertyFile("testing-github.properties").build()
     }
 
     @Test
@@ -25,25 +28,52 @@ class GitHubIssuesMojoTest {
         val repository = gitHub.getRepository("testingchooly/${TEST_REPOSITORY}")
 
         assertTrue(loadBody(repository).contains("test generated milestone"))
-
-        repository.findMilestoneByTitle("1.0.0")?.description = "updated description"
+        val milestone = repository.findMilestone("1.0.0")
+        milestone.description = "updated description"
         generator().generate()
         var body: String = loadBody(repository)
         assertFalse("Should not contain old milestone description", body.contains("automatically generated test project"))
         assertTrue("Should contain new milestone description", body.contains("updated description"))
-        assertFalse("Should not contain issues heading:\n$body", body.contains("Issues"))
+        assertFalse("Should not contain issues heading:\n$body", body.contains("Issues Resolved"))
+        assertFalse("Should not contain pr heading:\n$body", body.contains("Pull Requests Merged"))
 
+        repository.getIssues(OPEN, milestone)
+            .forEach { it.close() }
+        generator().generate()
+        body = loadBody(repository)
+        assertTrue("Should contain issues heading:\n$body", body.contains("Issues Resolved"))
+        assertFalse("Should not contain pr heading:\n$body", body.contains("Pull Requests Merged"))
+    }
 
+    @Test
+    fun ignoreInvalidIssues() {
+        createRepo()
+
+        val repository = gitHub.getRepository("testingchooly/${TEST_REPOSITORY}")
+        val milestone = repository.findMilestone("1.0.0")
+        repository.getIssues(OPEN, milestone)
+            .forEachIndexed { index, issue ->
+                if (issue.number == 2) {
+                    issue.addLabels("invalid")
+                }
+                issue.close()
+            }
+        generator().generate()
+        val body = loadBody(repository)
+        assertTrue("Should contain issues heading:\n$body", body.contains("Issues Resolved"))
+        assertTrue(body.contains("2 Issues Resolved"))
+        assertTrue(body.contains("[#1]"))
+        assertFalse(body.contains("[#2]"))
+        assertTrue(body.contains("[#3]"))
     }
 
     private fun loadBody(repository: GHRepository?): String {
-        return repository.findReleaseByName("Version 1.0.0")
-            ?.body
+        return repository.findReleaseByName("Version 1.0.0").body
             ?: throw IllegalStateException("Should have a body")
     }
 
-    private fun generator() = IssuesGenerator("testingchooly/" + TEST_REPOSITORY, "1.0.0-SNAPSHOT")
-        .config(File("../testing-github.properties"))
+    private fun generator() = IssuesGenerator("testingchooly/$TEST_REPOSITORY", "1.0.0-SNAPSHOT")
+        .config(File("testing-github.properties"))
 
     private fun createRepo() {
         try {
@@ -70,18 +100,6 @@ class GitHubIssuesMojoTest {
             .milestone(milestone)
             .create()
     }
-/*
-    @Test
-    fun ignoreInvalidIssues() {
-        val mojo = GitHubIssuesMojo
-                .build("MorphiaOrg/morphia", "1.5.0", "http://morphiaorg.github.io/morphia/1.5/javadoc/", outputDir = "target")
-        mojo.execute()
-
-        val file = File(mojo.outputDir, "Changes-1.5.0.md")
-        Assert.assertTrue(file.exists())
-        Assert.assertFalse(file.readText().contains("#1306"))
-    }
-*/
 /*
     @Test
     fun badMilestone() {
